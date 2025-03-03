9727ef14941364e8af108c3089ebec5c8c2433a4
Ayush Kumar Singh <ayushsingh17102000@gmail.com>
2025-03-03 17:50:08 +0530

    new file added

---
+++ JAVA SCRIPT/Revision of JS/script.js
@@ -0,0 +1,27 @@
+// Object literals
+const User = {
+    name: "Ayush",
+    email: "ayush@gmail.com",
+    age: 18,
+    location: "Noida-18"
+}
+console.log(User.email);
+console.log(User["email"]); // to access the email.
+
+let obj1 = {name:"chombi",age:16}
+
+let obj2 = { }
+// new empty object
+
+
+for (prop in obj1) {
+obj2[prop] = obj1[prop]
+console.log(obj2)
+}
+//copy key-values into new object
+
+obj2.age=20;
+console.log(obj2);
+//{ name:"chombi",age:20 }
+console.log(obj1);
+//{ name:"chombi",age:16 }
