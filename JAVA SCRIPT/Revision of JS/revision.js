aae67340b626b584e2a32ee3205040672de1613e
Ayush Kumar Singh <ayushsingh17102000@gmail.com>
2025-03-05 11:13:54 +0530

    revision of js

--- JAVA SCRIPT/Revision of JS/revision.js
+++ JAVA SCRIPT/Revision of JS/revision.js
@@ -1,2 +1,35 @@
 console.log("Hi Developers!!!!");
-alert(3+3) // it will run on browser but on node it will throw an error.
+//alert(3+3) // it will run on browser but on node it will throw an error.
+/** Data Types in JS
+ * 1. Number
+ * 2. String
+ * 3. Boolean
+ * 4. Objects
+ * 5. Bigint
+ * 6. Null :-> standalone value type of null is : Object.
+ * 7. Undefined
+ * 8. Symbol
+ 
+
+console.log(typeof null);       // Object
+console.log(typeof undefined);  // undefined
+*/
+
+/********************************************** Type Conversion******************************************* 
+
+let value= null             // it will give 0              //"33abc"           // Due to this it will throw NaN
+console.log(typeof value);
+console.log(typeof (value));
+
+let valuein=Number(value)
+console.log(typeof(valuein));
+console.log(valuein);       // NaN
+
+let loggedIn = 1
+console.log(loggedIn);
+let BooleanLoggedIn = Boolean(loggedIn)
+console.log(BooleanLoggedIn);           // true
+
+// 1:-> true 0:-> false ""-> false "ayush":-> true
+*/
+
